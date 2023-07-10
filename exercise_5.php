1bcf477c24205f7b1571ad34ff88fe6a92b9be4e
Loamy28 <rome.angelo15@gmail.com>
2023-07-10 16:28:53 +0800

    Update

--- exercise_5.php
+++ exercise_5.php
@@ -1,35 +1,25 @@
 <?php
-
-$start_num = 1;
-$end_num = 5;
-
-// Print the header of the table
 echo "<table border='1'>";
 echo "<tr>";
-echo "<th>Dividend</th>";
-echo "<th>Divisor</th>";
-echo "<th>Quotient</th>";
+
+    for ($column = 1; $column <= 10; $column++) {
+        echo "<th>$column</th>";
+    }
+
 echo "</tr>";
 
-// Loop through the dividend numbers
-for ($dividend = $start_num; $dividend <= $end_num; $dividend++) {
+    for ($row = 1; $row <= 10; $row++) {
+        echo "<tr>";
+        
 
-  // Loop through the divisor numbers
-  for ($divisor = $start_num; $divisor <= $end_num; $divisor++) {
+        for ($column = 1; $column <= 10; $column++) {
+            $result = $row / $column;
+            echo "<td>$result</td>";
+        }
 
-    // Calculate the quotient
-    $quotient = $dividend / $divisor;
+        echo "</tr>";
+    }
 
-    // Print the row of the table
-    echo "<tr>";
-    echo "<td>$dividend</td>";
-    echo "<td>$divisor</td>";
-    echo "<td>$quotient</td>";
-    echo "</tr>";
-  }
-}
-
-// Print the footer of the table
 echo "</table>";
 
 ?>
