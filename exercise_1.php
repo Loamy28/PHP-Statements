2ff12551de25cf89e7ac13df1db8d60762786486
Loamy28 <rome.angelo15@gmail.com>
2023-07-10 14:47:53 +0800

    First Commit

---
+++ exercise_1.php
@@ -0,0 +1,23 @@
+<?php
+    $word1 = 'class';
+    $word2 = 'kodego';
+    $word3 = 'top';
+
+    if(strlen($word1) > 5){
+        echo 'First word is more than 5 characters'.'<br>';
+    }else{
+        echo 'First word is less than 5 characters'.'<br>';
+    }
+
+    if(strlen($word2) > 5){
+        echo 'Second word is more than 5 characters'.'<br>';
+    }else{
+        echo 'Second word is less than 5 characters'.'<br>';
+    }
+
+    if(strlen($word3) > 5){
+        echo 'Third word is more than 5 characters'.'<br>';
+    }else{
+        echo 'Third word is less than 5 characters'.'<br>';
+    }
+?>
